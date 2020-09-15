538a20ce03470e248c2adf3bbef9a87e5a9325ad
Alphonse Bouy <alphonse.bouy@gmail.com>
2020-09-15 20:09:21 +0200

    clean

--- src/app.js
+++ src/app.js
@@ -11,129 +11,20 @@ const search = instantsearch({
   searchClient,
 });
 
-const index = searchClient.initIndex('perso_movies_carousel');
-
-// for (let index = 0; index < 20; index++) {
-//   aa('clickedFilters', {
-//     userToken: 'action_crime',
-//     index: 'perso_movies_carousel',
-//     eventName: 'added_to_watchlist_movie',
-//     filters: ['genre:Action'],
-//   });
-
-//   aa('viewedFilters', {
-//     userToken: 'action_crime',
-//     index: 'perso_movies_carousel',
-//     eventName: 'detailed_movie',
-//     filters: ['genre:Crime'],
-//   });
-// }
-
-// index
-//   .search('', {
-//     facetFilters: 'actors:Benedict Cumberbatch',
-//     hitsPerPage: 200,
-//   })
-//   .then(({ hits }) => {
-//     console.log(hits);
-//     hits.forEach((hit) => {
-//       if (Math.random() < 0.3) {
-//         aa('convertedObjectIDs', {
-//           eventName: 'watched_movie',
-//           index: 'perso_movies_carousel',
-//           objectIDs: [hit.objectID],
-//           userToken: 'history_cumberbatch',
-//         });
-//       }
-//       if (Math.random() < 0.5) {
-//         aa('clickedObjectIDs', {
-//           eventName: 'added_to_watchlist_movie',
-//           index: 'perso_movies_carousel',
-//           objectIDs: [hit.objectID],
-//           userToken: 'history_cumberbatch',
-//         });
-//       }
-//       if (Math.random() < 0.9) {
-//         aa('viewedObjectIDs', {
-//           eventName: 'detailed_movie',
-//           index: 'perso_movies_carousel',
-//           objectIDs: [hit.objectID],
-//           userToken: 'history_cumberbatch',
-//         });
-//       }
-//     });
-//   });
-// index
-//   .search('', {
-//     filters: 'genre:History',
-//     hitsPerPage: 200,
-//   })
-//   .then(({ hits }) => {
-//     console.log(hits);
-//     hits.forEach((hit) => {
-//       if (Math.random() < 0.3) {
-//         aa('convertedObjectIDs', {
-//           eventName: 'watched_movie',
-//           index: 'perso_movies_carousel',
-//           objectIDs: [hit.objectID],
-//           userToken: 'history_cumberbatch',
-//         });
-//       }
-//       if (Math.random() < 0.5) {
-//         aa('clickedObjectIDs', {
-//           eventName: 'added_to_watchlist_movie',
-//           index: 'perso_movies_carousel',
-//           objectIDs: [hit.objectID],
-//           userToken: 'history_cumberbatch',
-//         });
-//       }
-//       if (Math.random() < 0.9) {
-//         aa('viewedObjectIDs', {
-//           eventName: 'detailed_movie',
-//           index: 'perso_movies_carousel',
-//           objectIDs: [hit.objectID],
-//           userToken: 'history_cumberbatch',
-//         });
-//       }
-//     });
-//   });
-// index
-//   .search('', {
-//     facetFilters: ['genre: History'],
-//   })
-//   .then(({ hits }) => {
-//     console.log(hits);
-//     aa('convertedObjectIDs', {
-//       eventName: 'watched_movie',
-//       index: 'perso_movies_carousel',
-//       objectIDs: hits
-//         .filter((hit) => Math.random() < 0.3)
-//         .map((hit) => hit.objectID),
-//       userToken: 'history_cumberbatch',
-//     });
-//     aa('clickedObjectIDs', {
-//       eventName: 'added_to_watchlist_movie',
-//       index: 'perso_movies_carousel',
-//       objectIDs: hits
-//         .filter((hit) => Math.random() < 0.5)
-//         .map((hit) => hit.objectID),
-//       userToken: 'history_cumberbatch',
-//     });
-//     aa('viewedObjectIDs', {
-//       eventName: 'detailed_movie',
-//       index: 'perso_movies_carousel',
-//       objectIDs: hits
-//         .filter((hit) => Math.random() < 0.9)
-//         .map((hit) => hit.objectID),
-//       userToken: 'history_cumberbatch',
-//     });
-//   });
-// aa();
-
 const persoSelect = document.querySelector('#user_select');
 persoSelect.addEventListener('change', (e) => {
   window.location.href = `${window.location.origin}${window.location.pathname}?userToken=${e.target.value}`;
 });
+
+const searchParams = new URLSearchParams(window.location.search);
+
+const userToken = searchParams.get('userToken');
+const selectedOption = persoSelect.querySelector(
+  `option[value="${userToken}"]`
+);
+if (selectedOption) {
+  selectedOption.setAttribute('selected', true);
+}
 
 // Fetch from your own DB
 const referenceHits = {
@@ -186,30 +77,13 @@ const referenceHits = {
   },
 };
 
-const searchParams = new URLSearchParams(window.location.search);
-
-const userToken = searchParams.get('userToken');
-persoSelect
-  .querySelector(`option[value="${userToken}"]`)
-  ?.setAttribute('selected', true);
-
 const myCarousels = userToken
   ? [
-      // {
-      //   title: 'Trending',
-      //   context: 'trending',
-      //   indexName: 'perso_movies_carousel_trending',
-      // },
       {
         title: 'Most Popular',
         context: 'most-popular',
         indexName: 'perso_movies_carousel',
       },
-      // {
-      //   title: 'Last chance',
-      //   context: 'last-chance',
-      //   indexName: 'perso_movies_carousel_last_chance',
-      // },
 
       {
         title: 'Fantasy Comedy',
@@ -237,22 +111,11 @@ const myCarousels = userToken
       },
     ]
   : [
-      // {
-      //   title: 'Trending',
-      //   context: 'trending',
-      //   indexName: 'perso_movies_carousel_trending',
-      // },
       {
         title: 'Most Popular',
         context: 'most-popular',
         indexName: 'perso_movies_carousel',
       },
-      // {
-      //   title: 'Last chance',
-      //   context: 'last-chance',
-      //   indexName: 'perso_movies_carousel_last_chance',
-      // },
-
       {
         title: 'Fantasy Comedy',
         context: 'comedy_fantasy',
@@ -311,8 +174,3 @@ const addWidgets = () => {
 addWidgets();
 
 search.start();
-
-instantsearch.widgets.configure({
-  enablePersonalization: true,
-  userToken: 'myToken',
-});
